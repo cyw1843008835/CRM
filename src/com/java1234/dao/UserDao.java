daf41916a864a5b81d152ee8abc882cc18bdc782
18430 <18430@DESKTOP-DBENKHM>
2019-03-09 15:11:10 +0800

    commit

--- src/com/java1234/dao/UserDao.java
+++ src/com/java1234/dao/UserDao.java
@@ -1,20 +1,10 @@
 package com.java1234.dao;
 
-import com.java1234.entity.User;
-
 /**
  * 用户Dao接口
- * 
  * @author Administrator
  *
  */
 public interface UserDao {
 
-	/**
-	 * 用户登录
-	 * 
-	 * @param user
-	 * @return
-	 */
-	public User login(User user);
 }
--- src/com/java1234/service/UserService.java
+++ src/com/java1234/service/UserService.java
@@ -1,14 +1,10 @@
 package com.java1234.service;
 
-import com.java1234.entity.User;
-
 /**
  * 用户Service接口
- * 
  * @author Administrator
  *
  */
 public interface UserService {
-	public User login(User user);
 
 }
