a6eb12cef442f925105bc2eb5c04ac4c3c055493
18430 <18430@DESKTOP-DBENKHM>
2019-03-10 09:08:00 +0800

    section1-commit-1

--- src/com/java1234/dao/UserDao.java
+++ src/com/java1234/dao/UserDao.java
@@ -1,10 +1,19 @@
 package com.java1234.dao;
 
+import com.java1234.entity.User;
+
 /**
  * 用户Dao接口
+ * 
  * @author Administrator
  *
  */
 public interface UserDao {
-
+	/**
+	 * 用户登陆
+	 * 
+	 * @param user
+	 * @return
+	 */
+	public User login(User user);
 }
